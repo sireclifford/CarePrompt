ed62264d95e6c69db07ffff00b86ea8335ba7ea9
Clifford Owusu <ckowusu.engineer@gmail.com>
2026-07-01 04:02:59 -0600

    feat: add caregiver mode resident list with search and avatar

--- CarePrompt/ContentView.swift
+++ CarePrompt/ContentView.swift
@@ -1,15 +1,8 @@
-//
-//  ContentView.swift
-//  CarePrompt
-//
-//  Created by Clifford Owusu on 2026-06-30.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("CarePrompt")
+        ResidentListView()
     }
 }
 
